3b247a34b61471f9c645ab5b2b89c36fc1a8bc34
iceyisaak <me@iceyisaak.com>
2021-05-11 12:40:34 +0200

    Debugging color prop

--- components/Chat.js
+++ components/Chat.js
@@ -24,13 +24,14 @@ export default class Chat extends Component {
   }
 }
 
+
 const styles = StyleSheet.create({
 
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: `${this.props.route.params.color}`
+    backgroundColor: color
   }
 
 });
